ccf05eb62cc0c4c04af7014a6780dd8c25e3eaba
gyl30 <im.ggyyll@gmail.com>
2024-09-29 10:00:53 +0800

    Implement file handle

--- net/file_http_handle.cpp
+++ net/file_http_handle.cpp
@@ -5,5 +5,13 @@ namespace leaf
 
 void file_http_handle::handle(const leaf::http_session::ptr &session, const leaf::http_session::http_request_ptr &req)
 {
+    boost::beast::http::response<boost::beast::http::string_body> response;
+    response.result(boost::beast::http::status::ok);
+    response.set(boost::beast::http::field::content_type, "text/plain");
+    response.body() = "Hello, world!";
+    response.prepare_payload();
+    response.keep_alive(req->keep_alive());
+    auto msg = std::make_shared<boost::beast::http::message_generator>(std::move(response));
+    session->write(msg);
 }
 }    // namespace leaf
