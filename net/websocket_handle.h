8cff284a22849b51e9a6626684e3f3f64fa09ecc
gyl30 <im.ggyyll@gmail.com>
2024-09-30 21:36:00 +0800

    websocket session enable shared

--- net/websocket_handle.h
+++ net/websocket_handle.h
@@ -1,13 +1,14 @@
 #ifndef LEAF_WEBSOCKET_HANDLE_H
 #define LEAF_WEBSOCKET_HANDLE_H
 
+#include <memory>
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
 
 namespace leaf
 {
 
-class websocket_handle
+class websocket_handle : public std::enable_shared_from_this<websocket_handle>
 {
    public:
     using ptr = std::shared_ptr<websocket_handle>;
