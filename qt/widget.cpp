d34e3d6c8f49ece43671de75092b7c047b798c3b
gyl30 <im.ggyyll@gmail.com>
2024-12-06 21:15:06 +0800

    Add qt

--- qt/widget.cpp
+++ qt/widget.cpp
@@ -29,9 +29,22 @@ Widget::Widget(QWidget *parent) : QWidget(parent)
     layout->setContentsMargins(0, 0, 0, 0);
     setLayout(layout);
     resize(800, 300);
+    auto upload_cb = [this](const leaf::upload_event &e) { upload_progress(e); };
+    auto download_cb = [this](const leaf::download_event &e) { download_progress(e); };
+    file_client_ = new leaf::file_transfer_client("127.0.0.1", 8080, upload_cb, download_cb);
+    file_client_->startup();
+}
+void Widget::download_progress(const leaf::download_event &e)
+{
+    LOG_INFO("--> download progress {} {} {} {}", e.id, e.filename, e.download_size, e.file_size);
 }
 
-Widget::~Widget() = default;
+void Widget::upload_progress(const leaf::upload_event &e)
+{
+    LOG_INFO("<-- upload progress {} {} {} {}", e.id, e.filename, e.upload_size, e.file_size);
+}
+
+Widget::~Widget() { file_client_->shutdown(); }
 
 void Widget::on_new_file_clicked()
 {
--- qt/widget.h
+++ qt/widget.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include "table_view.h"
 #include "table_model.h"
+#include "file_transfer_client.h"
 
 class Widget : public QWidget
 {
@@ -15,8 +16,14 @@ class Widget : public QWidget
 
    private:
     void on_new_file_clicked();
+
+   private:
+    void upload_progress(const leaf::upload_event& e);
+    void download_progress(const leaf::download_event& e);
+
    private:
     leaf::task_model* model_ = nullptr;
     leaf::task_table_view* table_view_ = nullptr;
+    leaf::file_transfer_client* file_client_ = nullptr;
 };
 #endif    // WIDGET_H
