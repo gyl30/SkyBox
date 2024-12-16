218d703bfd17bdaa52c0140b75267a25dbc8b0c2
gyl30 <im.ggyyll@gmail.com>
2024-12-16 22:03:21 +0800

    Add clennup

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
+#include <absl/cleanup/cleanup.h>
 
-#include "widget.h"
+#include "log/log.h"
+#include "qt/widget.h"
 
 int main(int argc, char *argv[])
 {
+    leaf::init_log("gui.log");
+    leaf::set_log_level("trace");
+    auto cleanup = absl::MakeCleanup(leaf::shutdown_log);
+
     QApplication a(argc, argv);
     Widget w;
     w.show();
