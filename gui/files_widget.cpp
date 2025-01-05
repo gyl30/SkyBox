11d321274e87b5c9a9f4f24a95cd509d370a4ca7
gyl30 <im.ggyyll@gmail.com>
2025-01-05 21:08:30 +0800

    files widget 添加菜单

--- gui/files_widget.cpp
+++ gui/files_widget.cpp
@@ -1,5 +1,32 @@
+#include <QMenu>
+#include "log/log.h"
 #include "gui/files_widget.h"
+
 namespace leaf
 {
-files_widget::files_widget(QWidget *parent) : QTableWidget(parent) {}
+files_widget::files_widget(QWidget *parent) : QWidget(parent)
+{
+    rename_action_ = new QAction("重命名", this);
+    new_directory_action_ = new QAction("新建文件夹", this);
+    connect(rename_action_, &QAction::triggered, this, &files_widget::on_new_file_clicked);
+    connect(new_directory_action_, &QAction::triggered, this, &files_widget::on_new_directory_clicked);
+}
+files_widget::~files_widget()
+{
+    delete rename_action_;
+    delete new_directory_action_;
+}
+
+void files_widget::contextMenuEvent(QContextMenuEvent *event)
+{
+    QMenu menu(this);
+    menu.addAction(rename_action_);
+    menu.addAction(new_directory_action_);
+    menu.exec(event->globalPos());
+}
+
+void files_widget::on_new_file_clicked() { LOG_INFO("new file clicked"); }
+
+void files_widget::on_new_directory_clicked() { LOG_INFO("new directory clicked"); }
+
 }    // namespace leaf
--- gui/files_widget.h
+++ gui/files_widget.h
@@ -1,15 +1,28 @@
 #ifndef LEAF_GUI_FILES_WIDGET_H
 #define LEAF_GUI_FILES_WIDGET_H
 
-#include <QObject>
-#include <QTableWidget>
+#include <QWidget>
+#include <QAction>
+#include <QContextMenuEvent>
 
 namespace leaf
 {
-class files_widget : public QTableWidget
+class files_widget : public QWidget
 {
    public:
     explicit files_widget(QWidget *parent);
+    ~files_widget();
+
+   public:
+    void contextMenuEvent(QContextMenuEvent *event) override;
+
+   public:
+    void on_new_file_clicked();
+    void on_new_directory_clicked();
+
+   private:
+    QAction *rename_action_;
+    QAction *new_directory_action_;
 };
 }    // namespace leaf
 
