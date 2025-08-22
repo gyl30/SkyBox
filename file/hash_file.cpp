98ecb9c149ce5b7b5e0692412d39a5ebbe366f57
gyl30 <im.ggyyll@gmail.com>
2025-08-22 20:50:39 +0800

    refactor(file): 重构哈希文件函数并添加读取限制功能

--- file/hash_file.cpp
+++ file/hash_file.cpp
@@ -3,13 +3,13 @@
 #include <boost/system/error_code.hpp>
 
 #include "file/file.h"
-#include "file/hash_file.h"
 #include "crypt/blake2b.h"
+#include "file/hash_file.h"
 
 namespace leaf
 {
 
-std::string hash_file(const std::string& file, boost::system::error_code& ec)
+std::string hash_file(const std::string& file, boost::system::error_code& ec, std::size_t read_limit)
 {
     leaf::file_reader f(file);
     ec = f.open();
@@ -20,22 +20,31 @@ std::string hash_file(const std::string& file, boost::system::error_code& ec)
     leaf::blake2b b;
     constexpr auto kBufferSize = 4096;
     std::vector<uint8_t> buffer(kBufferSize, '0');
-    while (true)
+    std::size_t read_size = 0;
+    while (read_size < read_limit)
     {
-        auto read_size = f.read(buffer.data(), buffer.size(), ec);
+        auto r_size = std::min<std::size_t>(read_limit - read_size, kBufferSize);
+        auto len = f.read(buffer.data(), r_size, ec);
+
+        if (ec == boost::asio::error::eof)
+        {
+            ec = {};
+            if (len > 0)
+            {
+                b.update(buffer.data(), len);
+            }
+            break;
+        }
         if (ec)
         {
             break;
         }
-        b.update(buffer.data(), read_size);
+        b.update(buffer.data(), len);
+        read_size += len;
     }
+    b.final();
     auto close_ec = f.close();
-    if (ec == boost::asio::error::eof)
-    {
-        ec = {};
-        b.final();
-    }
-    else if (ec)
+    if (ec)
     {
         return {};
     }
--- file/hash_file.h
+++ file/hash_file.h
@@ -1,12 +1,13 @@
 #ifndef LEAF_FILE_HASH_FILE_H
 #define LEAF_FILE_HASH_FILE_H
 
+#include <limits>
 #include <string>
 #include <boost/system/error_code.hpp>
 
 namespace leaf
 {
-std::string hash_file(const std::string& file, boost::system::error_code& ec);
+std::string hash_file(const std::string& file, boost::system::error_code& ec, std::size_t read_limit = std::numeric_limits<std::size_t>::max());
 }
 
 #endif
